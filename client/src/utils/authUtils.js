c8bd01e723def55db6a3bc8e5b7099990e6968b9
Tim Quattrochi <timq82@gmail.com>
2023-09-01 17:12:51 -0400

    changed authDispatch to dispatch since I am using it in other areas

--- client/src/utils/authUtils.js
+++ client/src/utils/authUtils.js
@@ -3,14 +3,14 @@ import { APP_NAME } from "../config/constants";
 /**
  *
  * Dispatches an action to the provided dispatch function for updating the authentication state.
- * @param {Function} authDispatch - the dispatch function from the authReducer to update the state.
+ * @param {Function} dispatch - the dispatch function from the useReducer hook.
  * @param {string} caseToCall  - the action to dispatch.
  * @param {any} [value ] - the payload data for action (optional)
  * @returns {void}
  *
  */
-export const handleDispatch = (authDispatch, caseToCall, value) => {
-  authDispatch({
+export const handleDispatch = (dispatch, caseToCall, value) => {
+  dispatch({
     type: caseToCall,
     payload: value,
   });
